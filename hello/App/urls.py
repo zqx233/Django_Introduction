34d0b2640ccd366f4471f3692673c24416edc472
zhaoq <zhaoqixinmain@qq.com>
2021-01-12 20:19:19 +0800

    1

--- hello/App/urls.py
+++ hello/App/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
-
+# path第三个参数name是路由的名称，和视图函数参数无关
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'^tel/(?P<tel>\d{8})/$', views.get_phone, name='tel'),
+
+    # 响应对象
+    path('response/', views.handle_response, name='response'),
+
+    # 重定向
+    path('red/', views.handle_redirect, name='red'),
 ]
--- hello/App/views.py
+++ hello/App/views.py
@@ -1,8 +1,71 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello world")
+    # return HttpResponse("Hello world")
+    return render(request, "index.html", context={"title": 'Django', "name": '中国'})
+
+
+def get_phone(request, tel):
+    # request常用属性
+    # get传参的获取
+    print(request.GET)  # GET quersetDict，不能修改
+    # 获取单一值
+    print(request.GET.get('username'))
+    # 获取多值
+    print(request.GET.getlist('age'))  # 返回值是一个列表
+
+    # POST传参
+    print(request.POST.get('username'))
+    print(request.POST.getlist('hobby'))
+
+    # 获取请求方法
+    print(request.method)  # 返回方法是GET或POST　
+
+    # 获取请求路径
+    print(request.path)
+
+    # 其他请求属性
+    print(request.META)
+
+    # 客户端地址
+    print(request.META.get('REMOTE_ADDR'))
+    # 来源页面
+    print(request.META.get('HTTP_REFERER'))
+
+    # 常用方法
+    print(request.get_full_path())  # 请求路径＋查询字符串
+    print(request.get_host())  # 主机名和端口
+    print(request.build_absolute_uri())  # 完整的url
+
+    # 获取请求参数的字典 由queryDict转dict
+    print(request.GET.dict())
+
+    return HttpResponse(tel)
+
+
+def handle_response(request):
+    # res = HttpResponse("响应对象")
+    # res.content_type = "text/html"
+    # res.status_code = 400 # 人为设置状态码
+    # return res
+
+    # render返回响应对象
+    # res = render(request, 'index.html')
+    # return res
+
+    # jsonresponse 可以返回json字符串，一般时候可以把字典，列表转换为json返回给前端，字典，列表只能包含内置类型
+    # return JsonResponse({'name': 'tom'})
+    # 如果参数不是字典，必须把safe设置为false
+    return JsonResponse([1, 2, 3, 4, 5], safe=False)
+
+
+def handle_redirect(request):
+    # 重定向到指定路由地址，参数就是路由
+    # return HttpResponseRedirect('/app/')
+    # 可用缩写
+    return redirect('/app/')
